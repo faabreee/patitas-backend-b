c8f0f3f5d0a6bebef2aa3c6a02f1e5de1be82963
Fabree <reyescocafagardo@gmail.com>
2024-09-22 16:00:24 -0500

    invoque logic of controller's implementation

--- src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
@@ -1,19 +1,37 @@
 package pe.edu.cibertec.patitas_backend_b.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_backend_b.service.AutenticacionService;
 
 @RestController
 @RequestMapping("/autenticacion")
 public class AutenticacionController {
 
+    @Autowired
+    AutenticacionService autenticacionService;
+
     @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequest){
-        return new LoginResponseDTO("00", "", "Fabrizio Reyes", "reyescocafagardo@gmail.com");
+
+        try {
+            String[] datosusuario = autenticacionService.validarUsuario(loginRequest);
+            if (datosusuario == null){
+                return new LoginResponseDTO("01", "Error: Usuario no encontrado", "", "");
+            }
+            return new LoginResponseDTO("00", "", datosusuario[0], datosusuario[1]);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return new LoginResponseDTO("99", "Error: Ocurrio un problema", "", "");
+        }
+
     };
 
 }
